cfedd4ad5fe4bb7b77b65cae4e999affb8bfe051
Mukul P Markande <mukul@gooru.org>
2018-11-16 15:10:53 +0530

    NILE-3170: Add Student Activity Start Date for Class

--- src/main/java/org/gooru/nucleus/handlers/dataclass/api/constants/JsonConstants.java
+++ src/main/java/org/gooru/nucleus/handlers/dataclass/api/constants/JsonConstants.java
@@ -109,6 +109,8 @@ public final class JsonConstants {
   	public static final String COLLECTION_TYPE = "collectionType";
   	
   	public static final String TITLE = "title";
+  	
+  	public static final String START_DATE = "startDate";
 
 	private JsonConstants() {
 		throw new AssertionError();
