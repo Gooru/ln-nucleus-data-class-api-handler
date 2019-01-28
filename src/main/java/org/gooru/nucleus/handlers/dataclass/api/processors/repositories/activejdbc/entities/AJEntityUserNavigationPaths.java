6d173ac66c86204591fda2f0cf73d36a86ffe0a0
renuka <renuka.g@bambeeq.com>
2019-01-28 19:26:15 +0530

    GradableItemsAPI:system suggested content,check along with its hierarchy

--- src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/entities/AJEntityUserNavigationPaths.java
+++ src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/entities/AJEntityUserNavigationPaths.java
@@ -14,14 +14,16 @@ import org.javalite.activejdbc.annotations.Table;
 @DbName("coreDb")
 @Table("user_navigation_paths")
 public class AJEntityUserNavigationPaths extends Model {
+    
+    private static final String FETCH_SYSTEM_SUGGESTION = "suggested_content_id = ?::uuid and ctx_course_id = ?::uuid and ctx_lesson_id = ?::uuid";
 
-    public static AJEntityUserNavigationPaths fetchSystemSuggestedContent(String collId) throws Throwable {
+    public static AJEntityUserNavigationPaths fetchSystemSuggestedContent(String collId, String courseId, String lessonId) throws Throwable {
         DB coreDb = new DB("coreDb");
         try {
             coreDb.open(DataSourceRegistry.getInstance().getCoreDataSource());
             coreDb.connection().setReadOnly(true);
             coreDb.openTransaction();
-            LazyList<AJEntityUserNavigationPaths> results = AJEntityUserNavigationPaths.find("suggested_content_id = ?::uuid", collId);
+            LazyList<AJEntityUserNavigationPaths> results = AJEntityUserNavigationPaths.find(FETCH_SYSTEM_SUGGESTION, collId, courseId, lessonId);
             coreDb.commitTransaction();
             if (results != null && !results.isEmpty()) {
                 return results.get(0);
