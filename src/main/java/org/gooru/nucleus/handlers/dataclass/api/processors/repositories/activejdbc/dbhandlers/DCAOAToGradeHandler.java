17978faab57e9da1c266cdbb6efb2a883fb82841
RenukaGurumurthy <renuka.g@bambeeq.com>
2019-05-27 18:30:28 +0530

    Modifed OA to grade query to remove distinct based on date as per today's req assumption that "OA may not be scheduled twice in the same class"

--- src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/dbhandlers/DCAOAToGradeHandler.java
+++ src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/dbhandlers/DCAOAToGradeHandler.java
@@ -77,7 +77,7 @@ public class DCAOAToGradeHandler implements DBHandler {
     JsonObject result = new JsonObject();
     JsonArray resultArray = new JsonArray();
     Map<String, Integer> oaStudentMap = new HashMap<>();
-    Map<String, JsonObject> oaMap = new HashMap<>();
+    Map<String, JsonObject> sourceToRelay = new HashMap<>();
 
     List<Map> queMap =
         Base.findAll(AJEntityDailyClassActivity.GET_OA_TO_GRADE, this.classId);
@@ -88,28 +88,14 @@ public class DCAOAToGradeHandler implements DBHandler {
         String studentId = m.get(AJEntityDailyClassActivity.GOORUUID).toString();
         Date activityDate = Date.valueOf(m.get(AJEntityDailyClassActivity.DATE_IN_TIME_ZONE).toString());
 
-        JsonObject oaObject = new JsonObject();
-        oaObject.put(AJEntityDailyClassActivity.COLLECTION_OID, collectionId);
-        oaObject.put(AJEntityDailyClassActivity.COLLECTION_TYPE, collectionType);
-        oaObject.put(AJEntityDailyClassActivity.DATE_IN_TIME_ZONE, activityDate.toString());
-
-        if (!oaMap.containsKey(studentId+collectionId+activityDate.toString())) {
-          oaMap.put(studentId+collectionId+activityDate, oaObject);
-        }
-        // Find the latest Session Id for this User, for this resource in this CCUL
-        if (oaStudentMap.containsKey(studentId + collectionId + activityDate)) {
-          int studCount = oaStudentMap.get(studentId + collectionId + activityDate) + 1;
-          oaStudentMap.put(studentId + collectionId + activityDate, studCount);
-        } else {
-          oaStudentMap.put(studentId + collectionId + activityDate, 1);
-        }
+        calculateStudentCountAndGenerateSourceToRelay(oaStudentMap, sourceToRelay, collectionType,
+            collectionId, activityDate);
       });
-      buildResponse(resultArray, oaStudentMap, oaMap);
+      buildResponse(resultArray, oaStudentMap, sourceToRelay);
     } else {
       LOGGER.info("Offline Activity pending grading cannot be obtained");
     }
 
-    // TODO: Orchestrate Title etc
     result.put(JsonConstants.GRADE_ITEMS, resultArray).put(AJEntityDailyClassActivity.ATTR_CLASS_ID,
         this.classId);
 
@@ -118,11 +104,30 @@ public class DCAOAToGradeHandler implements DBHandler {
 
   }
 
+  private void calculateStudentCountAndGenerateSourceToRelay(Map<String, Integer> oaStudentMap,
+      Map<String, JsonObject> sourceToRelay, String collectionType, String collectionId,
+      Date activityDate) {
+    JsonObject oaObject = new JsonObject();
+    oaObject.put(AJEntityDailyClassActivity.COLLECTION_OID, collectionId);
+    oaObject.put(AJEntityDailyClassActivity.COLLECTION_TYPE, collectionType);
+    oaObject.put(AJEntityDailyClassActivity.DATE_IN_TIME_ZONE, activityDate.toString());
+
+    if (!sourceToRelay.containsKey(collectionId.toString())) {
+      sourceToRelay.put(collectionId, oaObject);
+    }
+    if (oaStudentMap.containsKey(collectionId)) {
+      int studCount = oaStudentMap.get(collectionId) + 1;
+      oaStudentMap.put(collectionId, studCount);
+    } else {
+      oaStudentMap.put(collectionId, 1);
+    }
+  }
+
   private void buildResponse(JsonArray resultArray, Map<String, Integer> oaStudentMap,
       Map<String, JsonObject> oaMap) {
     if (!oaMap.isEmpty()) {
-      oaMap.forEach((ky, val) -> {
-        if (oaStudentMap.containsKey(ky)) {
+      oaMap.forEach((key, val) -> {
+        if (oaStudentMap.containsKey(key)) {
           JsonObject coll = new JsonObject();
           String collId = val.getString(AJEntityDailyClassActivity.COLLECTION_OID); 
           String collTitle = fetchCollectionMeta(collId);
@@ -132,8 +137,8 @@ public class DCAOAToGradeHandler implements DBHandler {
               val.getString(AJEntityDailyClassActivity.COLLECTION_TYPE));
           coll.put(AJEntityDailyClassActivity.ACTIVITY_DATE,
               val.getString(AJEntityDailyClassActivity.DATE_IN_TIME_ZONE));
-          int studentCount = oaStudentMap.get(ky) != null
-              ? Integer.valueOf(oaStudentMap.get(ky).toString())
+          int studentCount = oaStudentMap.get(key) != null
+              ? Integer.valueOf(oaStudentMap.get(key).toString())
               : 0;
           coll.put(JsonConstants.STUDENT_COUNT, studentCount);
           resultArray.add(coll);
--- src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/entities/AJEntityDailyClassActivity.java
+++ src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/entities/AJEntityDailyClassActivity.java
@@ -569,7 +569,7 @@ public class AJEntityDailyClassActivity extends Model {
 
   
   public static final String GET_OA_TO_GRADE =
-      "SELECT distinct on (collection_id, actor_id, date_in_time_zone) session_id, collection_id, collection_type, actor_id, date_in_time_zone "
+      "SELECT collection_id, collection_type, actor_id, date_in_time_zone "
       + "from daily_class_activity where class_id = ? AND event_name = 'collection.play' AND event_type = 'stop' "
       + "AND collection_type = 'offline-activity' AND is_graded = 'false' AND grading_type = 'teacher' "
       + "order by collection_id, actor_id, date_in_time_zone, updated_at desc";
