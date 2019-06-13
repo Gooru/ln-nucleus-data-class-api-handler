64ae7aef962979ed3067334237bdafbf04085c23
am-gooru <ashish@gooru.org>
2019-06-13 11:15:19 +0530

    Grading pending does not depend on score being null. In db when grading pending records are stored with 0.0 value, they show up in listing API but do not show up in question grading API. Further there is a need to separate scoring from grading pending. If grading is pending, allow teacher to grade it, even if there is not null score present.

--- src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/dbhandlers/DCAStudentsForRubricQuestionsHandler.java
+++ src/main/java/org/gooru/nucleus/handlers/dataclass/api/processors/repositories/activejdbc/dbhandlers/DCAStudentsForRubricQuestionsHandler.java
@@ -1,5 +1,8 @@
 package org.gooru.nucleus.handlers.dataclass.api.processors.repositories.activejdbc.dbhandlers;
 
+import com.hazelcast.util.StringUtil;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import java.sql.Date;
 import java.util.List;
 import java.util.Map;
@@ -9,20 +12,16 @@ import org.gooru.nucleus.handlers.dataclass.api.processors.ProcessorContext;
 import org.gooru.nucleus.handlers.dataclass.api.processors.repositories.activejdbc.entities.AJEntityClassAuthorizedUsers;
 import org.gooru.nucleus.handlers.dataclass.api.processors.repositories.activejdbc.entities.AJEntityDailyClassActivity;
 import org.gooru.nucleus.handlers.dataclass.api.processors.responses.ExecutionResult;
+import org.gooru.nucleus.handlers.dataclass.api.processors.responses.ExecutionResult.ExecutionStatus;
 import org.gooru.nucleus.handlers.dataclass.api.processors.responses.MessageResponse;
 import org.gooru.nucleus.handlers.dataclass.api.processors.responses.MessageResponseFactory;
-import org.gooru.nucleus.handlers.dataclass.api.processors.responses.ExecutionResult.ExecutionStatus;
 import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.LazyList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.hazelcast.util.StringUtil;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 
 /**
  * @author mukul@gooru
- * 
  */
 public class DCAStudentsForRubricQuestionsHandler implements DBHandler {
 
@@ -62,7 +61,7 @@ public class DCAStudentsForRubricQuestionsHandler implements DBHandler {
           "Collection Id Missing. Cannot fetch student list"), ExecutionStatus.FAILED);
 
     }
-    
+
     String date = this.context.request().getString(MessageConstants.ACTIVITY_DATE);
     if (StringUtil.isNullOrEmpty(date)) {
       LOGGER.warn("Activity Date is mandatory to fetch student list");
@@ -79,14 +78,14 @@ public class DCAStudentsForRubricQuestionsHandler implements DBHandler {
   @Override
   @SuppressWarnings("rawtypes")
   public ExecutionResult<MessageResponse> validateRequest() {
-     List<Map> owner = Base.findAll(AJEntityClassAuthorizedUsers.SELECT_CLASS_OWNER,
-     this.context.request().getString(MessageConstants.CLASS_ID),
-     this.context.userIdFromSession());
-     if (owner.isEmpty()) {
-     LOGGER.debug("validateRequest() FAILED");
-     return new ExecutionResult<>(MessageResponseFactory.createForbiddenResponse(
-     "User is not authorized for Rubric Grading"), ExecutionStatus.FAILED);
-     }
+    List<Map> owner = Base.findAll(AJEntityClassAuthorizedUsers.SELECT_CLASS_OWNER,
+        this.context.request().getString(MessageConstants.CLASS_ID),
+        this.context.userIdFromSession());
+    if (owner.isEmpty()) {
+      LOGGER.debug("validateRequest() FAILED");
+      return new ExecutionResult<>(MessageResponseFactory.createForbiddenResponse(
+          "User is not authorized for Rubric Grading"), ExecutionStatus.FAILED);
+    }
 
     return new ExecutionResult<>(null, ExecutionStatus.CONTINUE_PROCESSING);
   }
@@ -95,36 +94,23 @@ public class DCAStudentsForRubricQuestionsHandler implements DBHandler {
   @SuppressWarnings({"rawtypes", "unchecked"})
   public ExecutionResult<MessageResponse> executeRequest() {
     JsonObject result = new JsonObject();
-    JsonArray resultarray = new JsonArray();
-    
-    LazyList<AJEntityDailyClassActivity> userIdforQue = AJEntityDailyClassActivity.findBySQL(
-        AJEntityDailyClassActivity.GET_DISTINCT_STUDENTS_FOR_THIS_RESOURCE, classId, collectionId,
-        context.questionId(), activityDate);
-    
-    if (!userIdforQue.isEmpty()) {
-      List<String> userIds = userIdforQue.collect(AJEntityDailyClassActivity.GOORUUID);
-      for (String userID : userIds) {
-        LOGGER.debug("UID is " + userID);
-        AJEntityDailyClassActivity scoreModel = AJEntityDailyClassActivity.findFirst(
-            "class_id = ? AND collection_id = ? AND resource_id = ? AND actor_id = ? "
-                + "AND event_name = 'collection.resource.play' AND event_type = 'stop' AND resource_type = 'question' "
-                + "AND grading_type = 'teacher' AND  date_in_time_zone = ? AND question_type = 'OE' order by updated_at desc",
-            classId, collectionId, context.questionId(), userID, activityDate);
-        if (scoreModel != null) {
-          Boolean isGraded = scoreModel.get(AJEntityDailyClassActivity.IS_GRADED) != null
-              ? Boolean.valueOf(scoreModel.get(AJEntityDailyClassActivity.IS_GRADED).toString())
-              : null;
-          if (scoreModel.get(AJEntityDailyClassActivity.SCORE) == null
-              && (isGraded != null && !isGraded)) {
-            resultarray.add(userID);
-          }
-        }
-      }
+    JsonArray studentsIdArray;
 
+    LazyList<AJEntityDailyClassActivity> userIdsPendingGradingforSpecifiedQ = AJEntityDailyClassActivity
+        .findBySQL(AJEntityDailyClassActivity.GET_DISTINCT_STUDENTS_FOR_THIS_RESOURCE, classId,
+            collectionId, context.questionId(), activityDate);
+
+    if (!userIdsPendingGradingforSpecifiedQ.isEmpty()) {
+      List<String> userIds = userIdsPendingGradingforSpecifiedQ
+          .collect(AJEntityDailyClassActivity.GOORUUID);
+      studentsIdArray = new JsonArray(userIds);
     } else {
-      LOGGER.info("Student list for this Rubric Question cannot be obtained");
+      studentsIdArray = new JsonArray();
+      LOGGER.info(
+          "No student list present in DB: class:'{}', collection:'{}', question: '{}', activity date: '{}'",
+          classId, collectionId, context.questionId(), activityDate);
     }
-    result.put(JsonConstants.STUDENTS, resultarray);
+    result.put(JsonConstants.STUDENTS, studentsIdArray);
     return new ExecutionResult<>(MessageResponseFactory.createGetResponse(result),
         ExecutionStatus.SUCCESSFUL);
   }
